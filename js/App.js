f74d2a0d87a68941e3b6fce896d238346828778d
Mybiss55 <44406758+Mybiss55@users.noreply.github.com>
2023-07-06 16:38:25 -0400

    Default Image Location Made

---
+++ js/App.js
@@ -0,0 +1,16 @@
+//Put all photos in an array
+let lrgPhotos = ['images\\flowers-pink-large.jpg', 'images\\flowers-purple-large.jpg', 'images\\flowers-red-large.jpg', 'images\\flowers-white-large.jpg', 'images\\flowers-yellow-large.jpg'];
+let smlPhotos = ['images\\flowers-pink-small.jpg', 'images\\flowers-purple-small.jpg', 'images\\flowers-red-small.jpg', 'images\\flowers-white-small.jpg', 'images\\flowers-yellow-small.jpg'];
+//Variable to track which photo is focused
+let focusedPhoto = lrgPhotos[0];
+//Create function that will put photos in proper locations in html
+function defaultImages(){
+    document.getElementById('focused-img').src = focusedPhoto;
+for(let i = 0; i < smlPhotos.length; i++){
+    document.getElementById('sml-img' + i).src = smlPhotos[i];
+}
+}
+defaultImages();
+//Change photo focused when clicked on
+
+//Maybe add a cool transition or something
